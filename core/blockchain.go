d1005d0044bef1352896f5d4deab612ad4d2ef56
songwen <songwen276@163.com>
2024-08-21 19:18:46 +0800

    定时获取topic配置

--- core/blockchain.go
+++ core/blockchain.go
@@ -19,10 +19,12 @@ package core
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"math/big"
+	"os"
 	"runtime"
 	"sort"
 	"sync"
@@ -2034,17 +2036,58 @@ func (bc *BlockChain) InsertChain(chain types.Blocks) (int, error) {
 	return bc.insertChain(chain, true)
 }
 
-var topicMap = map[string]string{
-	"0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822": "UniswapV2",
-	"0x2170c741c41531aec20e7c107c24eecfdd15e69c9bb0a8dd37b1840b9e0b207b": "Balancer",
-	"0x0874b2d545cb271cdbda4e093020c452328b24af12382ed62c4d00f5c26709db": "GMX",
-	"0xb2e76ae99761dc136e598d4a629bb347eccb9532a5f8bbd72e18467c3c34cc98": "PancakeStableSwap",
-	"0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67": "UniswapV3",
-	"0x19b47279256b2a23a1665c810c8d55a1758940ee09377d4f8d26497a3577dc83": "PancakeV3",
-	"0xa4228e1eb11eb9b31069d9ed20e7af9a010ca1a02d4855cee54e08e188fcc32c": "Smardex",
-	"0x0e8e403c2d36126272b08c75823e988381d9dc47f2f0a9a080d95f891d95c469": "WooPPV2",
-	"0xc2c0245e056d5fb095f04cd6373bc770802ebd1e6c918eb78fdef843cdb37b0f": "DSP",
-	"0x8b3e96f2b889fa771c53c981b40daf005f63f637f1869f707052d15a3dd97140": "CurveStableSwap",
+func init() {
+	fetchTopicMap()
+	go timerGetTopic()
+}
+
+func timerGetTopic() {
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			fetchTopicMap()
+		}
+	}
+}
+
+var topicMap = make(map[string]string)
+
+// {
+// 	"0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822": "UniswapV2",
+// 	"0x2170c741c41531aec20e7c107c24eecfdd15e69c9bb0a8dd37b1840b9e0b207b": "Balancer",
+// 	"0x0874b2d545cb271cdbda4e093020c452328b24af12382ed62c4d00f5c26709db": "GMX",
+// 	"0xb2e76ae99761dc136e598d4a629bb347eccb9532a5f8bbd72e18467c3c34cc98": "PancakeStableSwap",
+// 	"0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67": "UniswapV3",
+// 	"0x19b47279256b2a23a1665c810c8d55a1758940ee09377d4f8d26497a3577dc83": "PancakeV3",
+// 	"0xa4228e1eb11eb9b31069d9ed20e7af9a010ca1a02d4855cee54e08e188fcc32c": "Smardex",
+// 	"0x0e8e403c2d36126272b08c75823e988381d9dc47f2f0a9a080d95f891d95c469": "WooPPV2",
+// 	"0xc2c0245e056d5fb095f04cd6373bc770802ebd1e6c918eb78fdef843cdb37b0f": "DSP",
+// 	"0x8b3e96f2b889fa771c53c981b40daf005f63f637f1869f707052d15a3dd97140": "CurveStableSwap",
+// }
+
+func fetchTopicMap() error {
+	// 打开测试数据文件
+	file, err := os.Open("/blockchain/bsc/build/bin/topic.json")
+	if err != nil {
+		log.Error("Error opening topic file", "err", err)
+		return err
+	}
+	defer file.Close()
+
+	// 读取文件内容
+	fileContent, err := os.ReadFile("/blockchain/bsc/build/bin/topic.json")
+	if err != nil {
+		log.Error("Failed to read file", "err", err)
+	}
+
+	// 解析 JSON 文件内容到 map
+	err = json.Unmarshal(fileContent, &topicMap)
+	if err != nil {
+		log.Error("Failed to unmarshal JSON", "err", err)
+	}
+	return nil
 }
 
 // insertChain is the internal implementation of InsertChain, which assumes that
@@ -2350,15 +2393,15 @@ func (bc *BlockChain) insertChain(chain types.Blocks, setHead bool) (int, error)
 		for _, receipt := range receipts {
 			for _, reLog := range receipt.Logs {
 				// marshalLog, err := json.Marshal(reLog)
-				// log.Info("收据日志打印，", "reBlockNum", *receipt.BlockNumber, "logBlockNum", reLog.BlockNumber, "区块对应的收据receipt.Logs", marshalLog)
+				// log.Info("收据日志打印，", "logBlockNum", reLog.BlockNumber, "区块对应的收据receipt.Logs", marshalLog)
 				topics := reLog.Topics
 				for _, topic := range topics {
 					topicStr := "0x" + hex.EncodeToString(topic[:])
 					// log.Info("打印topic", "topic", topicStr)
 					topicOper := topicMap[topicStr]
 					if topicOper != "" {
-						address := hex.EncodeToString(reLog.Address[:])
-						log.Info("交易收据日志打印，", "reBlockNum", *receipt.BlockNumber, "logBlockNum", reLog.BlockNumber, "topic", topicStr, "topicOper", topicOper, "address", address)
+						address := "0x" + hex.EncodeToString(reLog.Address[:])
+						log.Info("交易收据日志打印，", "logBlockNum", reLog.BlockNumber, "topic", topicStr, "topicOper", topicOper, "address", address)
 					}
 				}
 			}
